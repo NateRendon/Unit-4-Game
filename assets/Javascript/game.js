2696723f40b8a727639715c44c53450df07866ff
Nathan Rendon <naterendon@gmail.com>
2019-08-15 17:25:15 -0500

    moved some folders to correct places and added fighters

---
+++ assets/Javascript/game.js
@@ -0,0 +1,21 @@
+const fighters = {
+    Alfred = {
+        hp = 120,
+        attack = 20,
+    },
+
+    DragonicOverlord = {
+        hp = 150,
+        attack = 18, 
+    },
+
+    BlondeEzel = {
+        hp = 100,
+        attack = 25,
+    },
+
+    PentagonalMagus = {
+        hp = 180,
+        attack = 15,
+    }
+}
